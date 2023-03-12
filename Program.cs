fb6b4766cb0de81c847c9cb6687f18b240c796dc
Carlos García Murillo <c.g.murillo91@gmail.com>
2023-03-12 17:01:13 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+class Program_Ficheros
+{
+    static void Main(string[] args)
+    {
+        string ruta = "C:/campions.txt";
+        string himno = "HIMNO DE TU EQUIPO FAVORITO";
+
+        // Comprobar si el archivo existe
+        if (File.Exists(ruta))
+        {
+            Console.WriteLine("HIMNO YA AÑADIDO");
+        }
+        else
+        {
+            // Crear el archivo y añadir el himno
+            File.WriteAllText(ruta, himno);
+            Console.WriteLine("Archivo creado y himno añadido.");
+        }
+
+        Console.ReadLine();
+    }
+}
